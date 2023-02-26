1da238fcce003ef4fff8f06fba54aa5eb7c4d658
Luis Macias Hau <luissmh150@gmail.com>
2023-02-25 20:15:23 -0600

    swager

--- Deleite.Api/Program.cs
+++ Deleite.Api/Program.cs
@@ -82,9 +82,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
+}
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
