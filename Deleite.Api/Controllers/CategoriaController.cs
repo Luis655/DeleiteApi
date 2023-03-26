b42fdab8618af0958606d774471bc871d0a880a1
Luis Macias Hau <luissmh150@gmail.com>
2023-03-26 02:10:06 -0600

    productos id?

--- Deleite.Api/Controllers/CategoriaController.cs
+++ Deleite.Api/Controllers/CategoriaController.cs
@@ -36,7 +36,7 @@ public class CategoriaController : ControllerBase
     }
 
     [HttpGet]
-    [Route("productos")]
+    [Route("productos/{id}")]
 
     public async Task<ActionResult> GetCategoriaConProductos(int id)
     {
